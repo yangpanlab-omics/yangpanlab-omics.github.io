3013bf195159832419b5b582940c3f6b4d7541f5
yangpanlab-omics <yangpan5@uic.edu>
2025-09-26 20:08:29 +0000

    Deploying to gh-pages from @ yangpanlab-omics/yangpanlab-omics.github.io@a81108483c4bab5d66abd9c62536eca77bf9de36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -499,14 +499,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%79%61%6E%67%70%61%6E%35@%75%69%63.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -520,14 +513,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=BXffGjIAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
